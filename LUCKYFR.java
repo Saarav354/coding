cc1353aee2ac31dfcbe62a37103b5a1bc4dd1702
KIRAN KUMAR SINGH <108235458+Saarav354@users.noreply.github.com>
2024-05-18 19:47:39 +0530

    Add files via upload

---
+++ LUCKYFR.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int t=sc.nextInt();
+		for(int i=0;i<t;i++){
+		    int c=0;
+		    int m=sc.nextInt();
+		    String str = Integer.toString(m);
+		    for(int j=0;j<str.length();j++){
+		    if(str.charAt(j)=='4'){
+		        c++;
+		    }}
+		    System.out.println(c);
+		    
+		}
+	
+	}
+}
